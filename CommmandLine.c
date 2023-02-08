a60487fa468054723435b5df0d04c0b94de9d707
James Miller <jam1211miller@gmail.com>
2023-02-08 09:23:53 -0700

    Added a README

--- CommmandLine.c
+++ CommmandLine.c
@@ -2,19 +2,33 @@
 // CST - 315
 // Project 1
 
+// To create and exectuable: gcc CommmandLine.c -o CommandLine
+// Run this executable: ./CommandLine
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
+char *userInput;
+
 void CommandLine()
 {
-    printf();
+    printf("Project 1: Command Line Interface \n");
+    printf("To leave type 1: \n");
+    printf("or \n");
+    printf("Enter a single command or a list of commands seperated by a ';':\n");
+}
+
+void handleInput()
+{
 }
 
 int main()
 {
+
+    CommandLine();
 
     return 0;
 }

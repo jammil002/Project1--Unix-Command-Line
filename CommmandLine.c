47ea6112c80956a6301d4f106e93a7eda90b8546
James Miller <jam1211miller@gmail.com>
2023-02-08 09:07:05 -0700

    First commit

---
+++ CommmandLine.c
@@ -0,0 +1,20 @@
+// James Miller
+// CST - 315
+// Project 1
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void CommandLine()
+{
+    printf();
+}
+
+int main()
+{
+
+    return 0;
+}

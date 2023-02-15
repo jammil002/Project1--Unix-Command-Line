0f1e14b7d13b2951f3d4fd20bd80924d8dfd5445
James Miller <jam1211miller@gmail.com>
2023-02-15 16:03:49 -0700

    Change to handle mutiple inputs and bash files

--- CommmandLine.cpp
+++ CommmandLine.cpp
@@ -1,117 +1,137 @@
-// James Miller
-// CST - 315
-// Project 1
-
-// ! To create and exectuable: gcc CommmandLine.c -o CommandLine
-// ! Run this executable: ./CommandLine
-
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <errno.h>
 #include <sys/wait.h>
 
-// Function Prototypes
-int HandleInput();
-void HandleTokenization(char userInput[]);
-void HandleExecute();
-void CommandLine();
+#define MAX_COMMANDS 10
 
-// Global Variables
-size_t total = 5;
-char *tokenArray[256];
-bool isFirstRun = true;
+void execute_commands(char *commands[], int num_commands);
+void handle_input(char *input);
+void batch_mode(FILE *batch_file);
+void end_execution();
+void command_line();
+void tokenize_input(char *input, char *tokens[], int *num_tokens);
 
-// Function that contains shell display:
-void CommandLine()
+int main(int argc, char **argv)
 {
-    if (isFirstRun)
+    if (argc > 1)
     {
-        printf("-------------------------------------------------------------\n");
-        printf("-             Project 1: Command Line Interface             -\n");
-        printf("-                   To leave type 'q' or                    -\n");
-        printf("-                Enter a single command or                  -\n");
-        printf("-           A list of commands seperated by a ';'           -\n");
-        printf("-------------------------------------------------------------\n");
-        isFirstRun = false;
+        FILE *batch_file = fopen(argv[1], "r");
+        if (batch_file)
+        {
+            batch_mode(batch_file);
+            fclose(batch_file);
+        }
+        else
+        {
+            fprintf(stderr, "Error opening batch file.\n");
+            exit(EXIT_FAILURE);
+        }
     }
-    printf("Turtle Shell >> ");
-    int quit = HandleInput();
+    else
+    {
+        command_line();
+    }
+    return 0;
+}
 
-    if (quit == 0)
+void execute_commands(char *commands[], int num_commands)
+{
+    pid_t pid;
+    int status;
+    int i;
+
+    for (i = 0; i < num_commands; i++)
+    {
+        char *command = commands[i];
+        pid = fork();
+        if (pid < 0)
+        {
+            fprintf(stderr, "Fork failed.\n");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+            char *tokens[MAX_COMMANDS];
+            int num_tokens = 0;
+            tokenize_input(command, tokens, &num_tokens);
+            if (execvp(tokens[0], tokens) < 0)
+            {
+                fprintf(stderr, "Error executing command: %s\n", tokens[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    while (wait(&status) > 0)
+    {
+        // Wait for child processes to complete.
+    }
+}
+
+void handle_input(char *input)
+{
+    char *tokens[MAX_COMMANDS];
+    int num_tokens = 0;
+    tokenize_input(input, tokens, &num_tokens);
+
+    if (num_tokens == 0)
     {
         return;
     }
 
-    CommandLine();
-}
-
-// Function scans user input then sends it to be tokenized:
-int HandleInput()
-{
-    size_t bufsize = 32;
-    char userInput[128];
-    int result = getline(userInput, &bufsize, stdin);
-
-    if (result == -1)
+    if (strcmp(tokens[0], "exit") == 0)
     {
-        printf("EOF detected, quitting...\n");
-        return 0;
-    }
-
-    if (userInput[1] == 'q')
-    {
-        return 0;
-    }
-
-    HandleTokenization(userInput);
-
-    return 1;
-}
-
-// Function splits up input into individual commands:
-void HandleTokenization(char userInput[])
-{
-    char *tokens;
-    int i = 0;
-
-    tokens = strtok(userInput, "\n ");
-    while (tokens != NULL)
-    {
-        tokenArray[i++] = tokens;
-        tokens = strtok(NULL, "\n ");
-    }
-    tokenArray[i] = NULL;
-    HandleExecute();
-}
-
-void HandleExecute()
-{
-    //** Fork creates a child process
-    int childStatus;
-    pid_t childProcess = fork();
-    if (childProcess == 0)
-    {
-        //** This is handled by the child process.
-        execvp(tokenArray[0], tokenArray);
-
-        //** If the execvp command returns, there must of been an error.
-        perror("Unknown command. \n");
-        exit(errno);
+        end_execution();
     }
     else
     {
-        //** This is then handled by the parent process.
-        //** We must wait for the child process to finish.
-        pid_t tpid = wait(&childStatus);
-
-        // After the waiting is complete the process will move on.
+        execute_commands(tokens, num_tokens);
     }
 }
 
-int main()
+void batch_mode(FILE *batch_file)
 {
-    CommandLine();
-    return 0;
+    char line[256];
+    while (fgets(line, sizeof(line), batch_file))
+    {
+        printf("%s", line);
+        handle_input(line);
+    }
+}
+
+void end_execution()
+{
+    printf("Exiting shell.\n");
+    exit(EXIT_SUCCESS);
+}
+
+void command_line()
+{
+    char input[256];
+    printf("-------------------------------------------------------------\n");
+    printf("-             Project 1: Command Line Interface             -\n");
+    printf("-                   To leave type 'q' or                    -\n");
+    printf("-                Enter a single command or                  -\n");
+    printf("-           A list of commands seperated by a ';'           -\n");
+    printf("-------------------------------------------------------------\n");
+    while (1)
+    {
+        printf("Turtle Shell >> ");
+        fgets(input, sizeof(input), stdin);
+        handle_input(input);
+    }
+}
+
+void tokenize_input(char *input, char *tokens[], int *num_tokens)
+{
+    char *token;
+    token = strtok(input, " ;\n");
+    while (token != NULL && *num_tokens < MAX_COMMANDS)
+    {
+        tokens[(*num_tokens)++] = token;
+        token = strtok(NULL, " ;\n");
+    }
+    tokens[*num_tokens] = NULL;
 }
